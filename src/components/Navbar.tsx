b52be4093d66f5aad7260f199b2af953ef946aef
Sanjana <sanjana6122002@gmail.com>
2025-03-11 18:01:56 +0530

    music website

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { HoveredLink, Menu, MenuItem, } from "./ui/navbar-menu";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
@@ -32,8 +32,8 @@ function Navbar({ className }: { className?: string }) {
             </HoveredLink>
           </div>
         </MenuItem>
-        <Link href="/contact">
-          <MenuItem setActive={setActive} active={active} item="Contact Us"> </MenuItem>
+        <Link href={"/contact"}>
+          <MenuItem setActive={setActive} active={active} item="Contact Us"></MenuItem>
         </Link>
       </Menu>
     </div>
